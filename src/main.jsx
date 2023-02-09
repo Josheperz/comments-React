f6e3299676fe800e8a0fd5b5f623de12eded0707
jose perez <josheperzng@gmail.com>
2023-02-09 17:26:32 -0400

    bugfix: setting on deploy in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './scss/index.scss'
-
+import {HashRouter, Routes, Route} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+  <HashRouter>
+    <Routes>
+        <Route path="/" element={<App/>}></Route>
+    </Routes>
+  </HashRouter>
   
-    <App />
+  </>
   
 )
