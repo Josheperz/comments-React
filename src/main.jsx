9f4ff8c770379418a3af30e41aa75aee3a12c9ff
jose perez <josheperzng@gmail.com>
2023-02-09 17:45:29 -0400

    bugfix:2

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './scss/index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {HashRouter, Routes, Route} from "react-router-dom";
+import { App } from './App';
+import './scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-  <HashRouter>
-    <Routes>
-        <Route path="/" element={<App/>}></Route>
-    </Routes>
-  </HashRouter>
-  
-  </>
+  <React.StrictMode>
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<App/>}></Route>
+        </Routes>
+    </HashRouter>
+  </React.StrictMode>
   
 )
